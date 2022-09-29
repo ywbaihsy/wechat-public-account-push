c27453ff9b68ce487ed53922de9012b1ddae3eb0
ywbaihsy <105191368+ywbaihsy@users.noreply.github.com>
2022-09-29 09:26:29 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -221,7 +221,7 @@ const USER_CONFIG = {
       // 一般都填自己
       name: '自己',
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'ooxZd5g6fTICFzKxunABDeFC-oEo',
     }
     // 你可以不断按格式往下增加
     // ...
